94bbf9b7417ec2ff9eca202d1432ebb26f23bc7d
MarkSmirnov13 <marksmirnov13@gmail.com>
2018-05-24 20:27:04 +0300

    work version!

---
+++ cmake-build-debug/CMakeFiles/ListOfTrees.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ListOfTrees.dir/main.cpp.o"
+  "ListOfTrees.pdb"
+  "ListOfTrees"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ListOfTrees.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
